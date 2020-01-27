61725c52db9dacf25b91fb05575d745b646bc7e4
선상원 <45645824+SeonSangWon@users.noreply.github.com>
2020-01-27 15:47:19 +0900

    댓글 수정 시, 해당 댓글 정보 조회 메서드 추가

--- Hyunjin/src/main/java/com/hyunjin/main/DAO/IReplyDAO.java
+++ Hyunjin/src/main/java/com/hyunjin/main/DAO/IReplyDAO.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import com.hyunjin.main.DTO.ReplyDTO;
 
-public interface IReplyDAOd {
+public interface IReplyDAO {
 
 	//게시글에 맞는 댓글 조회
 	public List<ReplyDTO> ReplyView(ReplyDTO replyDTO);
+
+	//댓글 수정 시, 해당 댓글 정보 조회
+	public List<ReplyDTO> ReplyOne(ReplyDTO replyDTO);
 	
 	//댓글 수정 시 필요한 비밀번호 유효성 검사
-	public ReplyDTO replyUpdatePassword(ReplyDTO replyDTO);
+	public ReplyDTO ReplyPassword(ReplyDTO replyDTO);
 	
 	//댓글 등록
 	public void ReplyInsert(ReplyDTO replyDTO);
